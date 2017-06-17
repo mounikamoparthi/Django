49bbcc0315fcf63050970063db6ea55e885bedb9
Mounika <mounika.tunga9@gmail.com>
2017-06-17 11:58:17 -0700

    added the route

--- display_time/apps/timedate/views.py
+++ display_time/apps/timedate/views.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
+#from __future__ import unicode_literals
 from django.shortcuts import render
+from datetime import datetime
 
-# Create your views here.
+def index(request):
+    context = {
+        'time': 'datetime.now()'
+    }
+    return render(request, 'timedate/index.html',context)
