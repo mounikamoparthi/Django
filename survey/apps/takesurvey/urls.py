68220e8121a01a076ff4941db635e73859e268f9
Mounika <mounika.tunga9@gmail.com>
2017-06-17 14:52:45 -0700

    logic for post method

--- survey/apps/takesurvey/urls.py
+++ survey/apps/takesurvey/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from .import views
 urlpatterns = [
     url(r'^$', views.index),
+    url(r'^surveys/process$', views.create),
     url(r'^result$', views.result)
 ]
--- survey/apps/takesurvey/views.py
+++ survey/apps/takesurvey/views.py
@@ -1,7 +1,21 @@
 # -*- coding: utf-8 -*-
 #from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render,redirect, HttpResponse
 
-def index(request)
-return render(request,'takesurvey/index.html')
+def index(request):
+    return render(request,'takesurvey/index.html')
+def create(request):
+    if request.method == "POST":
+        print '********'
+        print request.POST
+        request.session['Name'] = request.POST['name']
+        request.session['Location'] = request.POST['location']
+        request.session['Language'] = request.POST['language']
+        request.session['Comments'] = request.POST['comments']
+        return redirect('/result')
+    
+def result(request):
+    if request.method == "POST":
+        return redirect('/')
+    return render(request,'takesurvey/result.html')
