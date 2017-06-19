1ecd654318fca4cc04cf0810cf66184fdb71514d
Mounika <mounika.tunga9@gmail.com>
2017-06-19 15:22:50 -0700

    trying to show all ninjas in one page

--- disappearing_ninjas/apps/ninjas/urls.py
+++ disappearing_ninjas/apps/ninjas/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views           # So we can call functions from our routes!
 urlpatterns = [
 	url(r'^$', views.index),
+	url(r'^show$', views.showall),
 	url(r'^show/(?P<ninja_color>\w+)$', views.show)     # 'home' route.
 ]
  
--- disappearing_ninjas/apps/ninjas/views.py
+++ disappearing_ninjas/apps/ninjas/views.py
@@ -6,10 +6,39 @@ from django.shortcuts import render, HttpResponse, redirect
 def index(request):
 	#ninja got passed in through the url parameter!
 	return render(request, 'ninjas/index.html')
-def show(request, ninja_color):
-    if ninja_color == "red":
+
+def showall(request):
+    if  ninja_color is None:
         context = {
-            'ninja_color': '../../static/ninjas/images/leonardo.jpg'
+            'red': '../static/ninjas/images/raphael.jpg',
+            'blue': '../static/ninjas/images/leonardo.jpg',
+            'purple': '../static/ninjas/images/donatello.jpg',
+            'orange': '../static/ninjas/images/michelangelo.jpg'
+        }
+    return render(request, 'ninjas/showall.html',context)
+
+def show(request, ninja_color):
+    if ninja_color == "red" or ninja_color is None:
+        context = {
+            'ninja_color': '../static/ninjas/images/raphael.jpg'
+        }
+   
+    if ninja_color == "blue" or ninja_color is None:
+        context = {
+            'ninja_color': '../static/ninjas/images/leonardo.jpg'
+        }
+   
+    if ninja_color == "purple" or ninja_color is None:
+        context = {
+            'ninja_color': '../static/ninjas/images/donatello.jpg'
+        }
+    if ninja_color == "orange" or ninja_color is None:
+        context = {
+            'ninja_color': '../static/ninjas/images/michelangelo.jpg'
+        }
+    if ninja_color != 'red' and ninja_color !='blue' and ninja_color !='orange' and ninja_color !='purple':
+        context = {
+            'ninja_color': '../static/ninjas/images/notapril.jpg'
         }
     return render(request, 'ninjas/result.html',context)
     
