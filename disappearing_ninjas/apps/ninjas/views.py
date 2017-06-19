d964231f03e0199c4fab1ffffbdd6c66f3fc7859
Mounika <mounika.tunga9@gmail.com>
2017-06-19 11:10:11 -0700

    passing ninja through url

--- disappearing_ninjas/apps/ninjas/views.py
+++ disappearing_ninjas/apps/ninjas/views.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+#from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
 
-# Create your views here.
+def show_ninja(request, ninja):
+	#ninja got passed in through the url parameter!
+	context = {
+        'myninja' : ninja
+        }
+	return render(request, 'ninjas/showmyninja.html', context)
