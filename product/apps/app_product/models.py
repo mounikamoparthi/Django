9bb9b1c3a7ddf5252da278539f3cc376b4cc1113
Mounika <mounika.tunga9@gmail.com>
2017-06-20 15:46:39 -0700

    creates and saves the objects in database

--- product/apps/app_product/models.py
+++ product/apps/app_product/models.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+#from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=38)
+    greeting = models.CharField(max_length=38)
+    created_at = models.DateTimeField(auto_now_add=True)
+
--- product/apps/app_product/views.py
+++ product/apps/app_product/views.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+#from __future__ import unicode_literals
 
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Product
+def index(request):
+    Product.objects.create(name="Michael choi",greeting="Hiall")
+    product = Product.objects.all()
+    print product
+    return render(request, 'app_product/index.html')
