dd20a9b569de2fa55e2d961124a082065c3a150f
Mounika <mounika.tunga9@gmail.com>
2017-06-21 08:27:25 -0700

    initial commit

--- product/apps/app_product/views.py
+++ product/apps/app_product/views.py
@@ -5,6 +5,11 @@ from django.shortcuts import render
 from .models import Product
 def index(request):
     Product.objects.create(name="Michael choi",description="Hiall",weight = 4,Price= 20,Cost_to_seller= 19,category='vegetable')
-    product = Product.objects.all()
-    print product
+    output = Product.objects.filter(id =1)
+     
+    product1 = Product.objects.all()
+    for pp in product1:
+        print pp.name,pp.description,pp.weight,pp.Cost_to_seller,pp.Price,pp.category
+    print product1
+    print output 
     return render(request, 'app_product/index.html')
