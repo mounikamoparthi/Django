4bbe56ab3d3a33c3bd62baf46c67ccc01f046d94
Mounika <mounika.tunga9@gmail.com>
2017-06-21 13:37:32 -0700

    initial commit

--- books/apps/app_books/views.py
+++ books/apps/app_books/views.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+#from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .models import Books
 
-# Create your views here.
+def index(request):
+    Books.objects.create(title = " grokking algorithms", author = "AdityaY.Bhargava", category = "learners")
+    book = Books.object.all()
+    print book
+    return render (request,'app_books/index.html')
